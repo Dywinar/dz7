af1ae3af7fc89a3b6b2af86d772b510612469da3
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-26 20:45:29 +0300

    7.1.py

--- 7.1.py
+++ 7.1.py
@@ -1,4 +1,5 @@
 liste_1 = [1,3,4,5,4]
 liste_2 = [4,6,7,8,9]
-summ = tuple(map(lambda x, y: x + y,liste_1 , liste_2))
+summ = tuple(map(lambda x, y: x + y,liste_1 , liste_2)) # только тут не пойму почему когда я заворачиваю в list мне пвыводит ошибка а когда tuple все ок
 print(summ)
+
